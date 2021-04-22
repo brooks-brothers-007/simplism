832f2bd786cbaf7e00f3d4eabc67ed47025310e9
brooks-brothers-007 <shoutanakane@gmail.com>
2021-04-22 17:12:10 +0900

    simplismファイル生成

---
+++ app/helpers/simplism_helper.rb
@@ -0,0 +1,2 @@
+module SimplismHelper
+end
---
+++ test/controllers/simplism_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SimplismControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
